6398e54311fef67312101be0c2a942b411cb4897
Christian Perez <chris.dillon92@gmail.com>
2016-06-25 18:56:32 -0700

    reated board (not yet linked to DOM) + formats

---
+++ script.js
@@ -0,0 +1,36 @@
+var axis;
+var board = createBoard;
+var winner;
+// // Creates / Resets the Board // //
+function createBoard(){
+  var axis = [null, null, null];
+  var board = [axis, axis];
+  var winner = false;
+}
+
+
+// // Wire Up for Play // //
+
+console.log(board.length);
+// console.log('working2');
+
+
+// // Play // //
+
+// / assign value / //
+
+
+
+console.log("test1");
+  console.log("test2");
+  console.log("");
+
+// / check for winner / //
+// for (var i  = 0; i < board[1]; i++){
+//   //board[][] // function
+//   console.log("test2");
+// }
+//    que user  + style
+// if winner == true){
+//   console.log('a winner has been found');
+// }{else}{console.log('it worked')};
